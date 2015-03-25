c5b69bceef704b91a614e46c8d78a2aca9899621
Zach Himsel <zach@himsel.net>
2015-03-25 18:04:42 -0400

    Refactor install script
    Overall, this is a massive change. It uses symlinks instead of using
    $HOME itself (so you need to keep a separate directory for the repo
    instead of using $HOME.
    
    It also will build or delete vim's YouCompleteMe depending on user
    input.
    
    A lot of the symlinking logic was borrowed from the install script at
    captbaritone/dotfiles. Thanks!

--- install.sh
+++ install.sh
@@ -1,47 +1,147 @@
 #!/usr/bin/env bash
 
-cd "$(dirname $0)"
+# Check if old version of dotfiles is currently in use
+if [[ -e "$HOME/.git" ]]; then
+  echo "Existing ~/.git repo detected! Please remove the old repo before proceeding."
+  exit 1
+fi
 
-# install function
-function install_dotfiles() {
-  source=$(dirname $0)
-  echo "Copying from git repo path: $source"
-  cd $source
-  cp -aP .* ~/
-  cd ~
-  echo "Installing git submodules"
+# Get full path of current dir
+repo_path=$(cd $(dirname $0) ; pwd -P)
+
+# List of dotfiles that we'll be symlinking
+dotfiles=(\
+  bashrc \
+  gitconfig \
+  gitignore_global \
+  gnupg \
+  lftprc \
+  oh-my-zsh \
+  profile \
+  tmux.conf \
+  vim \
+  vimrc \
+  zsh \
+  zshrc \
+)
+
+# Prompt user for permission to continue
+function overwrite_check_prompt () {
+read -p "This MIGHT overwrite existing files in your home directory. Are you sure? (y/N) " -n 1
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  :
+else
+  echo "Exiting with no changes."
+  exit 2
+fi
+}
+
+# Ask user if they want scripts
+install_scripts_prompt() {
+  read -p "Would you like to install zhimsel/scripts, too? (y/N) " -n 1
+  echo ""
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    want_scripts=1
+  fi
+}
+
+# Pull down scripts repo and make ~/bin
+function install_scripts() {
+  if [[ $want_scripts == 1 ]]; then
+    echo "Cloning scripts repo into ~/scripts"
+    git clone https://github.com/zhimsel/scripts.git ~/scripts
+    mkdir -p ~/bin
+    echo "Manually symlink any scripts you want to use into ~/bin"
+  fi
+}
+
+# Generate the destination path of a dotfile
+function dot_path () {
+  echo "$HOME/.$1"
+}
+
+# Generate the destination path of a dotfile
+function local_path () {
+  echo "$HOME/.$1.local"
+}
+
+# Links the passed filename to its new location
+function link () {
+  local filename=$1
+  local path=$(dot_path "$filename")
+
+  # Check if source dotfile exists
+  if [[ ! -e "$filename" ]]; then
+    echo "Error: $filename doesn't exist."
+    return
+  fi
+
+  # Check if target exists, back up if so
+  if [[ -e $path ]] && [[ ! -L $path ]]; then
+    local localpath=$(local_path "$filename")
+    if [[ -e $localpath ]]; then
+      echo "Both $path and $localpath exist. Cannot do anything automatically. Please reconcile manually."
+    else
+      mv "$path" "$localpath"
+      echo "$path exists. Backing up to $localpath before symlinking."
+    fi
+  fi
+
+  # Actually link the source dotfile
+  if [[ -L $path ]]; then
+    echo "Link exists! $path"
+  elif [[ ! -e $path ]]; then
+    echo "Linking: $filename to $path"
+    ln -s "$repo_path"/"$filename" "$path"
+  fi
+}
+
+# Loops through and link all files without links
+function install_links () {
+  echo "Linking dotfiles into place:"
+  cd "$repo_path"
+  for file in "${dotfiles[@]}"
+  do
+    link $file
+  done
+}
+
+# Prompt if user wants to install 'YouCompleteMe'
+youcompleteme_prompt () {
+  read -p "Would you like to install and build YouCompleteMe for vim? (y/N) " -n 1
+  echo ""
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    want_youcompleteme=1
+  fi
+}
+
+# Install vim plugins
+install_vim_plugins () {
+  vim +BundleUpdate +qall
+  if [[ "$want_youcompleteme" == 1 ]]; then
+    cd "$HOME"/.vim/bundle/YouCompleteMe
+    ./install.sh
+  else
+    rm -rf "$HOME"/.vim/bundle/YouCompleteMe
+  fi
+}
+
+# Initialize git submodules
+init_submodules () {
+  echo "Initializing git submodules..."
+  cd "$repo_path"
   git submodule init
   git submodule update
-  echo "Installing vim plugins"
-  vim +PluginUpdate +qall
-  echo ""
 }
 
-function install_scripts() {
-  echo "Cloning scripts repo into ~/scripts"
-  git clone https://github.com/zhimsel/scripts.git ~/scripts
-  mkdir -p ~/bin
-  echo "Symlink any scripts you want to use into ~/bin"
-}
 
-# install scripts, too?
-read -p "Would you like to install your scripts, too? (y/n) " -n 1
+# Actually do it!
+overwrite_check_prompt
+install_scripts_prompt
+youcompleteme_prompt
 echo ""
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  scripts=1
-fi
-
-# prompt user for permission to continue
-read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-echo ""
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  install_dotfiles
-  if [[ $scripts == 1 ]]; then
-    install_scripts
-  fi
-fi
-echo ""
-
-# unset functions
-unset install_dotfiles;
-unset install_scripts;
+init_submodules
+install_links
+install_scripts
+install_vim_plugins
