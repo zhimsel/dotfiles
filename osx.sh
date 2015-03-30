d904bdbcd51caf5702020baba67665ad1314ee07
Zach Himsel <zach@himsel.net>
2015-03-30 16:14:44 -0400

    osx.sh: remove mail send key

--- osx.sh
+++ osx.sh
@@ -174,9 +174,6 @@ defaults write com.apple.dock expose-animation-duration -float 0.1
 # Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
 defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 
-# Add the keyboard shortcut ⌘ + Enter to send an email in Mail.app
-defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" -string "@\\U21a9"
-
 # Display emails in threaded mode, sorted by date (oldest at the top)
 defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
 defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortedDescending" -string "no"
