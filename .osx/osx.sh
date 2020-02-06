ac32d1ec8711885551246144e4788535d535ad23
Zach Himsel <zach@himsel.net>
2020-02-06 11:25:16 -0700

    Chrome: show "always open app" checkbox again

--- .osx/osx.sh
+++ .osx/osx.sh
@@ -264,3 +264,6 @@ defaults write com.google.Chrome.canary DisablePrintPreview -bool true
 # Expand the print dialog by default
 defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
 defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
+
+# Re-show the "Always open links with the app" checkbox
+defaults write com.google.Chrome ExternalProtocolDialogShowAlwaysOpenCheckbox -bool true
