986f4d7b679d20f1188ed02574543d38bbdf643d
Zach Himsel <zach@himsel.net>
2020-07-20 11:06:16 -0600

    Drop XDG exports from env.d
    It's already in .zshenv

--- .config/zsh/env.d/10-paths.zsh
+++ .config/zsh/env.d/10-paths.zsh
@@ -3,11 +3,6 @@ typeset -gxU path
 typeset -gxU manpath
 typeset -gxU fpath
 
-# Set XDG variables, if not already set
-export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
-export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
-export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
-
 # Manually override some program's paths to use XDG paths (that don't support them natively)
 export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/config"
 export ASDF_DATA_DIR="${XDG_DATA_HOME}/asdf"
