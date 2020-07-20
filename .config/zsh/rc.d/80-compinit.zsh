95925cbf856053549bb6b3ed2c9419b25c1a570b
Zach Himsel <zach@himsel.net>
2020-07-20 11:16:11 -0600

    Rework some completions
    - Move compdef's into their own file after compinit
    - Create completion for `dotu`

--- .config/zsh/rc.d/80-compinit.zsh
+++ .config/zsh/rc.d/80-compinit.zsh
@@ -1,9 +1,7 @@
-autoload -Uz +X compinit && compinit
-autoload -Uz +X bashcompinit && bashcompinit
-
-# Set some custom compdefs
-compdef tssh='ssh'
-compdef '_files -/ -W $HOME/dev' dev
+autoload -Uz +X compinit \
+  && compinit
+autoload -Uz +X bashcompinit \
+  && bashcompinit
 
 # This should be loaded after compinit
 zinit light "zsh-users/zsh-syntax-highlighting"
