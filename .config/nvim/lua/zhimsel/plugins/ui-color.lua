c5a90c4486810e637e813dd491f8a9d10cd3cbc9
Zach Himsel <zach@himsel.net>
2023-04-12 01:04:37 -0400

    WIP: lualine

--- .config/nvim/lua/zhimsel/plugins/ui-color.lua
+++ .config/nvim/lua/zhimsel/plugins/ui-color.lua
@@ -13,6 +13,36 @@ return {
   },
 
   -- better statusbar
+  -- {
+  --   'nvim-lualine/lualine.nvim',
+  --   enabled = true,
+  --   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  --   opts = {
+  --     options = {
+  --       theme = 'base16',
+
+  --       component_separators = '',
+  --       section_separators   = '',
+  --     },
+
+  --     sections = {
+  --       lualine_a = {'mode'},
+  --       lualine_b = { { 'filename', newfile_status = true, path = 4 } },
+  --       lualine_c = {'filename'},
+  --       lualine_x = {'encoding', 'fileformat', 'filetype'},
+  --       lualine_y = {'progress'},
+  --       lualine_z = {'location'}
+  --     },
+  --     inactive_sections = {
+  --       lualine_a = {},
+  --       lualine_b = {},
+  --       lualine_c = {'filename'},
+  --       lualine_x = {'location'},
+  --       lualine_y = {},
+  --       lualine_z = {}
+  --     },
+  --   },
+
   {
     -- https://github.com/vim-airline/vim-airline
     'vim-airline/vim-airline',
