5d89c79d64eed98390e8aba96326a409199f02de
Zach Himsel <zach.himsel@addepar.com>
2023-05-30 13:14:03 -0400

    vim: Fix f/F/t/T not being repeatable

--- .config/nvim/lua/zhimsel/plugins/textobj.lua
+++ .config/nvim/lua/zhimsel/plugins/textobj.lua
@@ -92,6 +92,10 @@ return {
       local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
       vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
       vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+      vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+      vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+      vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+      vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
 
     end
   },
