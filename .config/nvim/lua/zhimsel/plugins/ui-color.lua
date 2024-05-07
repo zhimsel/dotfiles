b0fd7372a468c16723773b15b3f0abcfe244c663
Zach Himsel <zach.himsel@addepar.com>
2024-05-07 16:01:16 -0400

    vim: Update indent-blankline settings for latest version

--- .config/nvim/lua/zhimsel/plugins/ui-color.lua
+++ .config/nvim/lua/zhimsel/plugins/ui-color.lua
@@ -119,9 +119,12 @@ return {
   -- https://github.com/lukas-reineke/indent-blankline.nvim
   {
     'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
     opts = {
-      show_current_context = true,
-    }
+      scope = {
+        enabled = true
+      }
+    },
   },
 
 }
