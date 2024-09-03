9c7dfd0d8debef90a9459b3eff7d571954bd9f6b
Zach Himsel <zach.himsel@addepar.com>
2024-09-03 10:05:51 -0400

    vim: Fix copilot-cmp indent issue
    This should be reverted when
    https://github.com/zbirenbaum/copilot-cmp/pull/116 is merged

--- .config/nvim/lua/zhimsel/plugins/github_copilot.lua
+++ .config/nvim/lua/zhimsel/plugins/github_copilot.lua
@@ -6,7 +6,9 @@ return {
     'zbirenbaum/copilot.lua',
     dependencies = {
       {
-        'zbirenbaum/copilot-cmp',
+        -- Revert when https://github.com/zbirenbaum/copilot-cmp/pull/116 is merged
+        'xlucn/copilot-cmp', commit = 'bc153996a7b76fbaa7307356ab99e85e0924b6f7',
+        -- 'zbirenbaum/copilot-cmp',
         dependencies = {
           { 'hrsh7th/nvim-cmp' },
         },
