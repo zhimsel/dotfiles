1cd99d1da63e6472b0f6359c15ea0ef005c37d01
Zach Himsel <zach@himsel.net>
2025-02-09 12:02:59 -0500

    zsh: Support asdf 0.16
    https://asdf-vm.com/guide/upgrading-to-v0-16

--- .config/zsh/profile.d/10-paths.zsh
+++ .config/zsh/profile.d/10-paths.zsh
@@ -1,3 +1,4 @@
 # Manually override some program's paths to use XDG paths (that don't support them natively)
 export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/config"
 export ASDF_DATA_DIR="${XDG_DATA_HOME}/asdf"
+path=("${ASDF_DATA_DIR}/shims" $path)
