705a1e639bcafc2389e929e11f231fe379e38fca
Zach Himsel <zach@himsel.net>
2026-05-24 09:39:37 -0400

    zsh: Put ASDF shims at the front of `$PATH`

--- .config/zsh/profile.d/90-paths.zsh
+++ .config/zsh/profile.d/90-paths.zsh
@@ -1,3 +1,6 @@
 # Add ~/bin to beginning of stack (mosty last) to allow
 # overriding any system-installed executables
 path=("$HOME/bin" $path)
+
+# ASDF shims need to go at the very beginning of the $PATH
+path=("${ASDF_DATA_DIR}/shims" $path)
